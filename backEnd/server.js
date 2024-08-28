823af0bcb0394d49a1be0dbdd9d3f9336c4eafb4
amine-232 <83480533+amine-232@users.noreply.github.com>
2024-08-28 17:15:53 +0100

    Update server.js
    new not

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const http = require("http");
-const webrtc = require("wrtc"); // Ensure you have 'wrtc' package installed
+const webrtc = require("wrtc"); // Ensure you have 'wrtc' package installed please check the new version with update code no need for wbrtc using peer to peer connection
 
 const socketIO = require("socket.io");
 const server = http.createServer(app);
