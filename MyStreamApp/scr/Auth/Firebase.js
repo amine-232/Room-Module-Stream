adb4943ce26bfb3bfd55e8e0fc8070eee89edce4
amine-232 <83480533+amine-232@users.noreply.github.com>
2025-02-23 14:17:34 +0000

    Update Firebase.js
    Add you own Firebase Configrations

--- MyStreamApp/scr/Auth/Firebase.js
+++ MyStreamApp/scr/Auth/Firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/database";
 // you can call it in the location where you are going to have the upload function
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-4nr7OR6kFKAM5Y53LgnlWNOAJjqa7Ws",
-  authDomain: "courseapp-d7d46.firebaseapp.com",
-  projectId: "courseapp-d7d46",
-  storageBucket: "courseapp-d7d46.appspot.com",
-  messagingSenderId: "1062568830626",
-  appId: "1:1062568830626:web:bd838110067b1e409399fb",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
